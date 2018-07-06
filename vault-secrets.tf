4bc335789933d65b0605ebc295eadad75312fd0b
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-06 14:43:39 +0100

    add some more testing secrets for the sftp module

--- vault-secrets.tf
+++ vault-secrets.tf
@@ -4,3 +4,15 @@ resource "azurerm_key_vault_secret" "jumpbox-username" {
   value = "test.bastion.user.name"
   vault_uri = "${module.sscs-vault.key_vault_uri}"
 }
+
+resource "azurerm_key_vault_secret" "admin-username" {
+  name = "admin-username"
+  value = "test.admin.user.name"
+  vault_uri = "${module.sscs-vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "admin-password" {
+  name = "admin-password"
+  value = "test.admin.user.name"
+  vault_uri = "${module.sscs-vault.key_vault_uri}"
+}
