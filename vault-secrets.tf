ed42a9d2a870faa4f8e0cb437011908e4e25515f
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-06 14:06:32 +0100

    add a stub value for bastion host to try to get things moving along

--- vault-secrets.tf
+++ vault-secrets.tf
@@ -1,6 +1,6 @@
 
-resource "azurerm_key_vault_secret" "bastion_host" {
-  name = "bastion_host"
-  value = "test.host.name"
+resource "azurerm_key_vault_secret" "jumpbox-username" {
+  name = "jumpbox-username"
+  value = "test.bastion.user.name"
   vault_uri = "${module.sscs-vault.key_vault_uri}"
 }
