4a7905eccd46c2cfb62ae51a88ded903072eb845
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-09 13:05:31 +0100

    remove vars not present in module

--- sftp.tf
+++ sftp.tf
@@ -2,12 +2,7 @@ module "sftp" {
   source                      = "git::git@github.com:hmcts/moj-module-sftp?ref=master"
   admin_username               = "${var.admin_username}"
   admin_password               = "${var.admin_password}"
-  bastion_host                 = "reformMgmtDevopsBastion.reform.hmcts.net"
   subnet_id                    = "${azurerm_subnet.mgmt-sftp-subnet.id}"
-  azurerm_location       = "UK South"
-  boot_diagnostics_storage_endpoint = "https://blob.windows~~~"
-  image_uri              = "PACKER IMAGE BUILD OUTPUT"
-  os_disk_storage_endpoint = "https://blob.windows~~~"
-  rg_name                = "keving-sftp-test-rg"
+  resource_group_name          = "${azurerm_resource_group.mgmt.name}"
   subnet_id              = "SOME SUBNET ID"
 }
