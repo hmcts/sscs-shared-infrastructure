489cbd830e6b9e7eacac879694977470677d4df1
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-24 16:00:51 +0100

    fight some more with intermediate file

--- derive-infra-config.sh
+++ derive-infra-config.sh
@@ -17,17 +17,18 @@
 # Debug, output commands.
 set -x
 
+EXPORT_PREFIX="env."
 AZURE_CONFIG_DIR="/opt/jenkins/.azure-$SUBSCRIPTION"
 
 export az_keyVault_name=infra-vault-nonprod
 export subscription_id=`az account show --query [id] -o tsv`
 
-echo TF_VAR_vault_uri="https://infra-vault-${ENVIRONMENT}.vault.azure.net/"
-echo TF_VAR_buildlog_sa_name="mgmtbuildlogstore${ENVIRONMENT}"
-echo TF_VAR_buildlog_sa_key=`az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${ENVIRONMENT} --output tsv | awk 'NR==1{ print \$3  }'`
-echo TF_VAR_consulclustersjson=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-jenkins-dnsforward --query value`
+echo ${EXPORT_PREFIX}TF_VAR_vault_uri="https://infra-vault-${ENVIRONMENT}.vault.azure.net/"
+echo ${EXPORT_PREFIX}TF_VAR_buildlog_sa_name="mgmtbuildlogstore${ENVIRONMENT}"
+echo ${EXPORT_PREFIX}TF_VAR_buildlog_sa_key=`az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${ENVIRONMENT} --output tsv | awk 'NR==1{ print \$3  }'`
+echo ${EXPORT_PREFIX}TF_VAR_consulclustersjson=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-jenkins-dnsforward --query value`
 
-echo diagnosticstore=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-log-store --query value`
+echo ${EXPORT_PREFIX}diagnosticstore=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-log-store --query value`
 
 
 
