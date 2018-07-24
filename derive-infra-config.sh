ad0b4684712114642bcc5861703dd2b1c9ae1829
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-24 15:01:35 +0100

    enquoting concerns again

--- derive-infra-config.sh
+++ derive-infra-config.sh
@@ -20,9 +20,9 @@ export subscription_id=`az account show --query [id] -o tsv`
 export TF_VAR_vault_uri="https://infra-vault-${ENVIRONMENT}.vault.azure.net/"
 export TF_VAR_buildlog_sa_name="mgmtbuildlogstore${ENVIRONMENT}"
 export TF_VAR_buildlog_sa_key=`az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${ENVIRONMENT} --output tsv | awk 'NR==1{ print \$3  }'`
-export TF_VAR_consulclustersjson=`az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-jenkins-dnsforward --query value`
+export TF_VAR_consulclustersjson=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-jenkins-dnsforward --query value`
 
-diagnosticstore=`az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-log-store --query value`
+diagnosticstore=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-log-store --query value`
 
 
 
