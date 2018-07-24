4fb39171eb83d476691d606366b5a41c698f92ce
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-24 16:08:25 +0100

    fix syntax and make the layout more sane

--- derive-infra-config.sh
+++ derive-infra-config.sh
@@ -14,21 +14,32 @@
 #
 # It draws heavily on moj-infrastructure-bootstrap/bootstrap.sh
 
+# Setup.
 # Debug, output commands.
 set -x
 
 EXPORT_PREFIX="env."
 export AZURE_CONFIG_DIR="/opt/jenkins/.azure-$SUBSCRIPTION"
 
+# Derive values.
 az_keyVault_name=infra-vault-nonprod
 subscription_id=`az account show --query [id] -o tsv`
 
-echo ${EXPORT_PREFIX}TF_VAR_vault_uri="https://infra-vault-${ENVIRONMENT}.vault.azure.net/"
-echo ${EXPORT_PREFIX}TF_VAR_buildlog_sa_name="mgmtbuildlogstore${ENVIRONMENT}"
-echo ${EXPORT_PREFIX}TF_VAR_buildlog_sa_key=`az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${ENVIRONMENT} --output tsv | awk 'NR==1{ print \$3  }'`
-echo ${EXPORT_PREFIX}TF_VAR_consulclustersjson=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-jenkins-dnsforward --query value`
+TF_VAR_vault_uri="https://infra-vault-${ENVIRONMENT}.vault.azure.net/"
+TF_VAR_buildlog_sa_name="mgmtbuildlogstore${ENVIRONMENT}"
+export TF_VAR_buildlog_sa_name
+TF_VAR_buildlog_sa_key=`az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${ENVIRONMENT} --output tsv | awk 'NR==1{ print \$3  }'`
+TF_VAR_consulclustersjson=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-jenkins-dnsforward --query value`
 
-echo ${EXPORT_PREFIX}diagnosticstore=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-log-store --query value`
+diagnosticstore=`az keyvault secret show --vault-name ${az_keyVault_name} --name cfg-log-store --query value`
 
-
+# Output vars for use in intermediate file.
+for VARNAME in \
+   TF_VAR_vault_uri \
+   TF_VAR_buildlog_sa_name \
+   TF_VAR_buildlog_sa_key \
+   TF_VAR_consulclustersjson \
+; do 
+   echo ${EXPORT_PREFIX}$VARNAME="${$VARNAME}"
+done
 
