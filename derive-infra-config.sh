81448676e7904db4a520a13712227a8b67437345
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-24 13:48:48 +0100

    apply jenkinsfile alias into shell script to avoid login issues

--- derive-infra-config.sh
+++ derive-infra-config.sh
@@ -10,14 +10,16 @@
 #
 # It draws heavily on moj-infrastructure-bootstrap/bootstrap.sh
 
+alias az-env "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION az"
+
 az_keyVault_name="infra-vault-nonprod"
-export subscription_id=`az account show --query [id] -o tsv`
+export subscription_id=`az-env account show --query [id] -o tsv`
 export TF_VAR_vault_uri="https://infra-vault-${ENVIRONMENT}.vault.azure.net/"
 export TF_VAR_buildlog_sa_name="mgmtbuildlogstore${ENVIRONMENT}"
-export TF_VAR_buildlog_sa_key=`az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${ENVIRONMENT} --output tsv | awk 'NR==1{ print \$3  }'`
-export TF_VAR_consulclustersjson=`az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-jenkins-dnsforward --query value`
+export TF_VAR_buildlog_sa_key=`az-env storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${ENVIRONMENT} --output tsv | awk 'NR==1{ print \$3  }'`
+export TF_VAR_consulclustersjson=`az-env keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-jenkins-dnsforward --query value`
 
-diagnosticstore=`az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-log-store --query value`
+diagnosticstore=`az-env keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-log-store --query value`
 
 
 
