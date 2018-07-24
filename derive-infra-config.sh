29774ed82449c1de6f7404eb68d8b84aa42d9b58
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-24 13:29:57 +0100

    make assignments bash format compatible

--- derive-infra-config.sh
+++ derive-infra-config.sh
@@ -10,14 +10,14 @@
 #
 # It draws heavily on moj-infrastructure-bootstrap/bootstrap.sh
 
-az_keyVault_name = "infra-vault-nonprod"
-export subscription_id = `az account show --query [id] -o tsv`
-export TF_VAR_vault_uri = "https://infra-vault-${environment}.vault.azure.net/"
-export TF_VAR_buildlog_sa_name = "mgmtbuildlogstore${environment}"
-export TF_VAR_buildlog_sa_key = `az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${environment} --output tsv | awk 'NR==1{ print \$3  }'`
-export TF_VAR_consulclustersjson = `az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-jenkins-dnsforward --query value`
+az_keyVault_name="infra-vault-nonprod"
+export subscription_id=`az account show --query [id] -o tsv`
+export TF_VAR_vault_uri="https://infra-vault-${environment}.vault.azure.net/"
+export TF_VAR_buildlog_sa_name="mgmtbuildlogstore${environment}"
+export TF_VAR_buildlog_sa_key=`az storage account keys list --subscription ${subscription_id} --account-name ${TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${environment} --output tsv | awk 'NR==1{ print \$3  }'`
+export TF_VAR_consulclustersjson=`az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-jenkins-dnsforward --query value`
 
-diagnosticstore = `az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-log-store --query value`
+diagnosticstore=`az keyvault secret show --vault-name \"${az_keyVault_name}\" --name cfg-log-store --query value`
 
 
 
