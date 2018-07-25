79b56f4e02af595330521bc1711c2f9f1241b745
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-25 12:29:20 +0100

    sort another var

--- derived-infra-config.groovy
+++ derived-infra-config.groovy
@@ -30,4 +30,5 @@
  env.TF_VAR_sftp_client_id = "test-id-for-now"
  env.TF_VAR_sftp_client_secret = "test-secret-for-now"
  env.TF_VAR_sftp_ssh_authorized_key = "test-authorized-key-for-now"
+ env.TF_VAR_sftp_ssh_priv_key = "test-private-key-for-now"
 
