e0f03b36be7e8fe2e150485dbdb5c0bc8052ff6a
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-25 11:51:08 +0100

    sort another var

--- derived-infra-config.groovy
+++ derived-infra-config.groovy
@@ -22,4 +22,4 @@
  env.TF_VAR_owaspdb_password = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name OWASPDb-Password"
  env.TF_VAR_pipelinemetrics_cosmosdb_key = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name pipelinemetrics-cosmosdb-key"
  sa_rg = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name cfg-state-store | jq .rg_name"
- env.TF_VAR_resource_group_name = "${sa_rg_name}-${environment}"
+ env.TF_VAR_resource_group_name = "${sa_rg}-${environment}"
