bc75cb8265bf414632e3e9bf618e3c7a9e961702
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-25 11:37:31 +0100

    sort another var

--- derived-infra-config.groovy
+++ derived-infra-config.groovy
@@ -19,4 +19,5 @@
  env.TF_VAR_github_enterprise_apikey = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name github-enterprise-apikey"
  env.TF_VAR_hashicorp_vault_token = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name hashicorp-vault-token"
  env.TF_VAR_hmcts_github_apikey = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name hmcts-github-apikey"
+ env.TF_VAR_owaspdb_password = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name OWASPDb-Password"
  env.TF_VAR_pipelinemetrics_cosmosdb_key = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name pipelinemetrics-cosmosdb-key"
