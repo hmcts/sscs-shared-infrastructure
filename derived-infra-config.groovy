07bb27fec6db4fdb57ae9bae6337263e9cfedebf
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-25 10:45:05 +0100

    syntax fix

--- derived-infra-config.groovy
+++ derived-infra-config.groovy
@@ -2,13 +2,13 @@
 
  def az = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az $cmd", returnStdout: true).trim() }
 
- env.az_keyvault_name = "infra-vault-nonprod"
+ env.az_keyVault_name = "infra-vault-nonprod"
  subscription_id = az "account show --query [id] -o tsv"
  env.TF_VAR_vault_uri = "https://infra-vault-${environment}.vault.azure.net/"
  env.TF_VAR_buildlog_sa_name = "mgmtbuildlogstore${environment}"
  env.TF_VAR_buildlog_sa_key = az "storage account keys list --subscription ${subscription_id} --account-name ${env.TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${environment} --output tsv | awk 'NR==1{ print \$3  }'"
  env.TF_VAR_consulclustersjson = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name cfg-jenkins-dnsforward --query value"
- dg = az "keyvault secret show cfg-log-store | jq -r .rg_name"
+ dg = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" cfg-log-store | jq -r .rg_name"
  dg_name = "${dg}-${environment}"
  env.TF_VAR_diagnostics_sa_endpoint = "https://${dg_name}.blob.core.windows.net"
 
