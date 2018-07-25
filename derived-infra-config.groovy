7803aca661ce1afeac4cbf9bf516064265d9b8eb
Kevin Green <Kevin.Green@HMCTS.NET>
2018-07-25 10:48:49 +0100

    syntax fix

--- derived-infra-config.groovy
+++ derived-infra-config.groovy
@@ -8,8 +8,8 @@
  env.TF_VAR_buildlog_sa_name = "mgmtbuildlogstore${environment}"
  env.TF_VAR_buildlog_sa_key = az "storage account keys list --subscription ${subscription_id} --account-name ${env.TF_VAR_buildlog_sa_name} --resource-group mgmt-buildlog-store-${environment} --output tsv | awk 'NR==1{ print \$3  }'"
  env.TF_VAR_consulclustersjson = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name cfg-jenkins-dnsforward --query value"
- dg = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" cfg-log-store | jq -r .rg_name"
+ dg = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name cfg-log-store | jq -r .rg_name"
  dg_name = "${dg}-${environment}"
  env.TF_VAR_diagnostics_sa_endpoint = "https://${dg_name}.blob.core.windows.net"
 
- env.TF_VAR_github_apikey = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" hmcts-github-apikey"
+ env.TF_VAR_github_apikey = az "keyvault secret show --vault-name \"${env.az_keyVault_name}\" --name hmcts-github-apikey"
