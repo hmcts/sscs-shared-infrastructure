866f859437ae74d5c09c6e41be421be2e70895ed
Daniele Polencic <daniele.polencic@gmail.com>
2018-08-06 17:03:15 +0100

    added vault

---
+++ key-vault.tf
@@ -0,0 +1,14 @@
+module "sscs-vault" {
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name = "sscs-${var.env}"
+  product = "${var.product}"
+  env = "${var.env}"
+  tenant_id = "${var.tenant_id}"
+  object_id = "${var.jenkins_AAD_objectId}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  product_group_object_id = "87099fce-881e-4654-88d2-7c36b634e622"
+}
+
+output "vaultName" {
+  value = "${module.sscs-vault.key_vault_name}"
+}
