45668a50f6e5374704cc0fe4c21c365d2488ef0f
Daniele Polencic <daniele.polencic@gmail.com>
2018-08-07 11:23:24 +0100

    added missing variables

--- key-vault.tf
+++ key-vault.tf
@@ -1,11 +1,32 @@
+variable "tenant_id" {
+  type        = "string"
+  description = "The Tenant ID of the Azure Active Directory"
+}
+
+variable "product" {
+  type        = "string"
+  description = "The name of your application"
+  default     = "sscs"
+}
+
+variable "jenkins_AAD_objectId" {
+  type        = "string"
+  description = "This is the ID of the Application you wish to give access to the Key Vault via the access policy"
+}
+
+variable "env" {
+  type        = "string"
+  description = "The deployment environment (sandbox, aat, prod etc..)"
+}
+
 module "sscs-vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name = "sscs-${var.env}"
-  product = "${var.product}"
-  env = "${var.env}"
-  tenant_id = "${var.tenant_id}"
-  object_id = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = "sscs-${var.env}"
+  product                 = "${var.product}"
+  env                     = "${var.env}"
+  tenant_id               = "${var.tenant_id}"
+  object_id               = "${var.jenkins_AAD_objectId}"
+  resource_group_name     = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "87099fce-881e-4654-88d2-7c36b634e622"
 }
 
