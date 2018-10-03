c929191fa43f2a568eaf9ff775c628a2cd2d5ed2
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-10-03 09:10:23 +0100

    SSCS-4071 Added main.tf and updated .gitignore file

---
+++ main.tf
@@ -0,0 +1,8 @@
+locals {
+  common_tags = {
+    "environment"  = "${var.env}"
+    "Team Name"    = "${var.team_name}"
+    "Team Contact" = "${var.team_contact}"
+    "Destroy Me"   = "${var.destroy_me}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,21 @@ variable "location" {
   type    = "string"
   default = "UK South"
 }
+
+variable "team_name" {
+  type        = "string"
+  description = "Team name"
+  default     = "SSCS"
+}
+
+variable "team_contact" {
+  type        = "string"
+  description = "Team contact"
+  default     = "#sscs-sta"
+}
+
+variable "destroy_me" {
+  type        = "string"
+  description = "Here be dragons! In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
+  default     = "No"
+}
