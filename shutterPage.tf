bf501877ac1566afa69a645961bf95b21a266e2b
Toni sanchez <toninos@gmail.com>
2019-02-25 12:53:53 +0000

    Adding shutterPage terraform file

---
+++ shutterPage.tf
@@ -0,0 +1,41 @@
+variable "shutterPageDirectory" {
+    type    = "string"
+    default = "shutterPages"
+}
+
+module "SYAshutterPage" {
+    source                = "git@github.com:hmcts/moj-module-sya-shutterpage?ref=master"
+    location              = "${azurerm_resource_group.rg.location}"
+    env                   = "${var.env}"
+    resource_group_name   = "${azurerm_resource_group.rg.name}"
+    shutterPageDirectory  = "${var.shutterPageDirectory}/sya"
+    tag_list              = "${local.common_tags}"
+    product               = "${var.product}"
+    subscription          = "${var.subscription}"
+    shutterCustomDomain   = "${var.product}"
+}
+
+module "TYAshutterPage" {
+    source                = "git@github.com:hmcts/moj-module-tya-shutterpage?ref=master"
+    location              = "${azurerm_resource_group.rg.location}"
+    env                   = "${var.env}"
+    resource_group_name   = "${azurerm_resource_group.rg.name}"
+    shutterPageDirectory  = "${var.shutterPageDirectory}/tya"
+    tag_list              = "${local.common_tags}"
+    product               = "${var.product}"
+    subscription          = "${var.subscription}"
+    shutterCustomDomain   = "${var.product}"
+}
+
+module "CORshutterPage" {
+    source                = "git@github.com:hmcts/moj-module-cor-shutterpage?ref=master"
+    location              = "${azurerm_resource_group.rg.location}"
+    env                   = "${var.env}"
+    resource_group_name   = "${azurerm_resource_group.rg.name}"
+    shutterPageDirectory  = "${var.shutterPageDirectory}/cor"
+    tag_list              = "${local.common_tags}"
+    product               = "${var.product}"
+    subscription          = "${var.subscription}"
+    shutterCustomDomain   = "${var.product}"
+}
+
