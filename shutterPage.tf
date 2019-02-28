17977850372e3c22d5ddb86f00cd3089ff294801
Constantin <34478619+constantinpr@users.noreply.github.com>
2019-02-28 15:15:52 +0000

    Apply fix for custom shutter pages. (#23)
    * Update and rename shutterPage.disabled to shutterPage.tf
    
    * Update shutterPage.tf

--- shutterPage.tf
+++ shutterPage.tf
@@ -1,34 +1,34 @@
-module "SYAshutterPage" {
-    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=master"
+module "shutterPage" {
+    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
     location              = "${azurerm_resource_group.rg.location}"
     env                   = "${var.env}"
     resource_group_name   = "${azurerm_resource_group.rg.name}"
     shutterPageDirectory  = "${var.shutterPageDirectory}/sya"
-    tag_list              = "${local.common_tags}"
+    tag_list              = "${var.common_tags}"
     product               = "${var.product}"
     subscription          = "${var.subscription}"
     shutterCustomDomain   = "${var.product}"
 }
 
-module "TYAshutterPage" {
-    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=master"
+module "shutterPage" {
+    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
     location              = "${azurerm_resource_group.rg.location}"
     env                   = "${var.env}"
     resource_group_name   = "${azurerm_resource_group.rg.name}"
     shutterPageDirectory  = "${var.shutterPageDirectory}/tya"
-    tag_list              = "${local.common_tags}"
+    tag_list              = "${var.common_tags}"
     product               = "${var.product}"
     subscription          = "${var.subscription}"
     shutterCustomDomain   = "${var.product}"
 }
 
-module "CORshutterPage" {
-    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=master"
+module "shutterPage" {
+    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
     location              = "${azurerm_resource_group.rg.location}"
     env                   = "${var.env}"
     resource_group_name   = "${azurerm_resource_group.rg.name}"
     shutterPageDirectory  = "${var.shutterPageDirectory}/cor"
-    tag_list              = "${local.common_tags}"
+    tag_list              = "${var.common_tags}"
     product               = "${var.product}"
     subscription          = "${var.subscription}"
     shutterCustomDomain   = "${var.product}"
