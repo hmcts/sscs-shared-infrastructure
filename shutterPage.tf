d96eafff7e4032befa6267a6e8d66ba290ec32a5
Constantin <34478619+constantinpr@users.noreply.github.com>
2019-02-28 15:26:25 +0000

    Update shutterPage.tf

--- shutterPage.tf
+++ shutterPage.tf
@@ -9,28 +9,3 @@ module "shutterPage" {
     subscription          = "${var.subscription}"
     shutterCustomDomain   = "${var.product}"
 }
-
-module "shutterPage" {
-    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
-    location              = "${azurerm_resource_group.rg.location}"
-    env                   = "${var.env}"
-    resource_group_name   = "${azurerm_resource_group.rg.name}"
-    shutterPageDirectory  = "${var.shutterPageDirectory}/tya"
-    tag_list              = "${var.common_tags}"
-    product               = "${var.product}"
-    subscription          = "${var.subscription}"
-    shutterCustomDomain   = "${var.product}"
-}
-
-module "shutterPage" {
-    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
-    location              = "${azurerm_resource_group.rg.location}"
-    env                   = "${var.env}"
-    resource_group_name   = "${azurerm_resource_group.rg.name}"
-    shutterPageDirectory  = "${var.shutterPageDirectory}/cor"
-    tag_list              = "${var.common_tags}"
-    product               = "${var.product}"
-    subscription          = "${var.subscription}"
-    shutterCustomDomain   = "${var.product}"
-}
-
