b0096edfe05769a0c0a37e4b193329e10d155dd8
Constantin <34478619+constantinpr@users.noreply.github.com>
2019-03-05 14:16:23 +0000

    Shutterpagefix (#30)
    * individual pages added
    
    * module shutter page renamed 
    
    * subscription var added
    
    * custom domains changes
    
    * typo fixed
    
    * individual pages added
    
    * module shutter page renamed 
    
    * subscription var added
    
    * custom domains changes
    
    * typo fixed
    
    * product name changed for the shutter pages
    
    * changed shutter custom domain

--- shutterPageCOR.tf
+++ shutterPageCOR.tf
@@ -1,12 +1,11 @@
 module "shutterPageCOR" {
-    source                = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
-    location              = "${azurerm_resource_group.rg.location}"
-    env                   = "${var.env}"
-    resource_group_name   = "${azurerm_resource_group.rg.name}"
-    shutterPageDirectory  = "${var.shutterPageDirectory}/cor"
-    tag_list              = "${var.common_tags}"
-    product               = "${var.product}-cor-frontend"
-    subscription          = "${var.subscription}"
-    shutterCustomDomain   = "${var.product}-cor-frontend"
+  source               = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
+  location             = "${azurerm_resource_group.rg.location}"
+  env                  = "${var.env}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  shutterPageDirectory = "${var.shutterPageDirectory}/cor"
+  tag_list             = "${var.common_tags}"
+  product              = "${var.product}-cor-frontend"
+  subscription         = "${var.subscription}"
+  shutterCustomDomain  = "${replace(var.product, "-cor-frontend", "")}"
 }
-
