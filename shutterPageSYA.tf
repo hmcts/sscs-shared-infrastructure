e125aec08a4a9c9871f8eb776e415c6bf5694242
Chris Moreton <christopher.moreton@hmcts.net>
2019-03-13 11:02:51 +0000

    Comment out shutter pages which break build

--- shutterPageSYA.tf
+++ shutterPageSYA.tf
@@ -1,11 +1,11 @@
-module "shutterPageSYA" {
-  source               = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
-  location             = "${azurerm_resource_group.rg.location}"
-  env                  = "${var.env}"
-  resource_group_name  = "${azurerm_resource_group.rg.name}"
-  shutterPageDirectory = "${var.shutterPageDirectory}/sya"
-  tag_list             = "${var.common_tags}"
-  product              = "${var.product}-tribunals-frontend"
-  subscription         = "${var.subscription}"
-  shutterCustomDomain  = "${replace(var.product, "-tribunals-frontend", "")}"
-}
+//module "shutterPageSYA" {
+//  source               = "git@github.com:hmcts/moj-module-shutterpage?ref=CNP-585"
+//  location             = "${azurerm_resource_group.rg.location}"
+//  env                  = "${var.env}"
+//  resource_group_name  = "${azurerm_resource_group.rg.name}"
+//  shutterPageDirectory = "${var.shutterPageDirectory}/sya"
+//  tag_list             = "${var.common_tags}"
+//  product              = "${var.product}-tribunals-frontend"
+//  subscription         = "${var.subscription}"
+//  shutterCustomDomain  = "${replace(var.product, "-tribunals-frontend", "")}"
+//}
