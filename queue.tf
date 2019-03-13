fca96374fc8851ec39a5f256c7db5fd0e9f4a33a
Chris Moreton <christopher.moreton@hmcts.net>
2019-03-13 10:59:14 +0000

    Use topics instead of queues

--- queue.tf
+++ queue.tf
@@ -1,4 +1,4 @@
-module "queue-namespace" {
+module "servicebus-namespace" {
   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace.git"
   name                = "${var.product}-servicebus-${var.env}"
   location            = "${var.location}"
@@ -7,39 +7,9 @@ module "queue-namespace" {
   common_tags         = "${var.common_tags}"
 }
 
-module "evidenceshare-queue" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue.git"
-  name                = "evidenceshare"
-  namespace_name      = "${module.queue-namespace.name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-
-  requires_duplicate_detection            = "true"
-  duplicate_detection_history_time_window = "PT1H"
-  lock_duration                           = "PT5M"
-  max_delivery_count                      = "288" // To retry processing the message for 24hours
-}
-
-module "notifications-queue" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue.git"
-  name                = "notifications"
-  namespace_name      = "${module.queue-namespace.name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  lock_duration       = "PT5M"
-  max_delivery_count  = "288" // To retry processing the message for 24hours
-}
-
-output "evidenceshare_queue_primary_listen_connection_string" {
-  value = "${module.evidenceshare-queue.primary_listen_connection_string}"
-}
-
-output "evidenceshare_queue_primary_send_connection_string" {
-  value = "${module.evidenceshare-queue.primary_send_connection_string}"
-}
-
-output "notifications_queue_primary_listen_connection_string" {
-  value = "${module.notifications-queue.primary_listen_connection_string}"
-}
-
-output "notifications_queue_primary_send_connection_string" {
-  value = "${module.notifications-queue.primary_send_connection_string}"
+module "evidenceshare-topic" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic.git"
+  name                  = "evidenceshare"
+  namespace_name        = "${module.servicebus-namespace.name}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
 }
