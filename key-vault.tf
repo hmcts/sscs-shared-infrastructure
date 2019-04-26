42560510b5db5cb1aeaa5a54c195c01ef1b58547
Chris Moreton <christopher.moreton@hmcts.net>
2019-04-26 13:03:06 +0100

    Add common_tags to sscs-prod module

--- key-vault.tf
+++ key-vault.tf
@@ -17,6 +17,7 @@ module "sscs-vault" {
   object_id               = "${var.jenkins_AAD_objectId}"
   resource_group_name     = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "87099fce-881e-4654-88d2-7c36b634e622"
+  common_tags             = "${local.tags}"
 }
 
 output "vaultName" {
