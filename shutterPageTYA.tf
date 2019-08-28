8abfa463e9f8daca0a8645cd63f137affbf15ee7
Milan0140 <40658124+Milan0140@users.noreply.github.com>
2019-08-28 16:52:50 +0100

    comment-current-domain

--- shutterPageTYA.tf
+++ shutterPageTYA.tf
@@ -7,5 +7,5 @@ module "shutterPageTYA" {
   tag_list             = "${var.common_tags}"
   product              = "${var.product}-tya-frontend"
   subscription         = "${var.subscription}"
-  shutterCustomDomain  = "${replace(var.product, "-tya-frontend", "")}"
+  //shutterCustomDomain  = "${replace(var.product, "-tya-frontend", "")}"
 }
