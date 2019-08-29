badadecf71f7a16ed7960b93e637019f533bc08a
Chris <christopher.davidson@btinternet.com>
2019-08-29 10:35:32 +0100

    added gateway for ithc mya

--- app-gateway.tf
+++ app-gateway.tf
@@ -11,7 +11,9 @@ data "azurerm_subnet" "subnet_a" {
 
 locals {
   tribunals_frontend_suffix  = "${var.env != "prod" ? "-tribunals-frontend" : ""}"
+  mya_suffix  = "${var.env != "prod" ? "-cor-frontend" : ""}"
   tribunals_frontend_internal_hostname  = "${var.product}-tribunals-frontend-${var.env}.service.core-compute-${var.env}.internal"
+  mya_internal_hostname  = "${var.product}-cor-frontend-${var.env}.service.core-compute-${var.env}.internal"
 
   rfe_internal_hostname = "${var.product}-rfe-${var.env}.service.core-compute-${var.env}.internal"
 }
@@ -39,6 +41,11 @@ module "appGw" {
       data     = "${data.azurerm_key_vault_secret.tribunals_frontend_cert.value}"
       password = ""
     },
+    {
+      name     = "${var.mya_external_cert_name}${local.mya_suffix}"
+      data     = "${data.azurerm_key_vault_secret.tribunals_frontend_cert.value}"
+      password = ""
+    },
   ]
 
   # Http Listeners
@@ -50,6 +57,14 @@ module "appGw" {
       Protocol                = "Https"
       SslCertificate          = "${var.tribunals_frontend_external_cert_name}${local.tribunals_frontend_suffix}"
       hostName                = "${var.tribunals_frontend_external_hostname}"
+    },
+    {
+      name                    = "${var.product}-https-listener-palo-mya"
+      FrontendIPConfiguration = "appGatewayFrontendIP"
+      FrontendPort            = "frontendPort443"
+      Protocol                = "Https"
+      SslCertificate          = "${var.mya_external_cert_name}${local.mya_suffix}"
+      hostName                = "${var.mya_external_hostname}"
     },
   ]
 
@@ -83,6 +98,13 @@ module "appGw" {
       backendAddressPool  = "${var.product}-${var.env}-backend-palo"
       backendHttpSettings = "backend-80-palo"
     },
+    {
+      name                = "http-palo-mya"
+      ruleType            = "Basic"
+      httpListener        = "${var.product}-https-listener-palo-mya"
+      backendAddressPool  = "${var.product}-${var.env}-backend-palo"
+      backendHttpSettings = "backend-80-palo"
+    },
   ]
 
   probes = [
@@ -96,6 +118,17 @@ module "appGw" {
       backendHttpSettings = "backend-80-palo"
       healthyStatusCodes  = "200"
       host                = "${local.tribunals_frontend_internal_hostname}"
+    },
+    {
+      name                = "http-probe-palo-mya"
+      protocol            = "Http"
+      path                = "/health"
+      interval            = 30
+      timeout             = 30
+      unhealthyThreshold  = 5
+      backendHttpSettings = "backend-80-palo"
+      healthyStatusCodes  = "200"
+      host                = "${local.mya_internal_hostname}"
     },
   ]
 }
