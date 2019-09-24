8ec4199b9e46f1896128756a618151fdf49b69d1
Ryan Da Costa <ryan.dacosta@hmcts.net>
2019-09-24 16:56:29 +0100

    SSCS-5210 - get the right name

--- servicebus.tf
+++ servicebus.tf
@@ -1,7 +1,7 @@
 locals {
-  notifications_subscription_name = "${var.product}-notifications-topic-${var.env}"
   evidenceshare_topic_name        = "${var.product}-evidenceshare-topic-${var.env}"
   evidenceshare_subscription_name = "${var.product}-evidenceshare-subscription-${var.env}"
+  notifications_subscription_name = "${var.product}-notifications-subscription-${var.env}"
   servicebus_namespace_name       = "${var.product}-servicebus-${var.env}"
   resource_group_name             = "${azurerm_resource_group.rg.name}"
 }
