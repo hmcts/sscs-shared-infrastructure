1f5450ae27812a3207b907ee7c5b92057fc2ad28
sunil <sunil.rochani@hmcts.net>
2020-07-21 13:29:47 +0100

    SSCS-7804: Tech debt – Migrate over to new managed identities for accessing keyvaults

--- main.tf
+++ main.tf
@@ -3,7 +3,12 @@ provider "azurerm" {
 }
 
 locals {
-  tags = "${merge(var.common_tags,
-    map("Team Contact", "#sscs")
+  tags = "${
+    merge(
+    var.common_tags,
+    map(
+        "Team Contact", "#sscs",
+        "Team Name", "SSCS"
+        )
     )}"
 }
