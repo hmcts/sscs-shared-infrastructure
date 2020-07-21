816165e3bb715a4deeb47ec8e81cde1e56c748e4
sunil <sunil.rochani@hmcts.net>
2020-07-21 12:48:57 +0100

    SSCS-7804: Tech debt – Migrate over to new managed identities for accessing keyvaults

--- key-vault.tf
+++ key-vault.tf
@@ -10,8 +10,8 @@ module "sscs-vault" {
   common_tags             = "${local.tags}"
   location                = "${var.location}"
 
-  managed_identity_object_id = "${var.managed_identity_object_id}"
-
+  managed_identity_object_id = ["${var.managed_identity_object_id}"]
+  create_managed_identity    = true
 }
 
 data "azurerm_key_vault" "sscs_key_vault" {
