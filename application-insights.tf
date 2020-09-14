7c9609430d47a0e82a7cd5077937b378459bc68d
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2020-09-14 11:41:41 +0100

    Upgrade terraform

--- application-insights.tf
+++ application-insights.tf
@@ -1,21 +1,19 @@
 variable "appinsights_location" {
-  type        = "string"
   default     = "West Europe"
   description = "Location for Application Insights"
 }
 
 variable "appinsights_application_type" {
-  type        = "string"
   default     = "Web"
   description = "Type of Application Insights (Web/Other)"
 }
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
-  location            = "${var.appinsights_location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  application_type    = "${var.appinsights_application_type}"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = var.appinsights_application_type
 }
 
 output "appInsightsInstrumentationKey" {
-  value = "${azurerm_application_insights.appinsights.instrumentation_key}"
+  value = azurerm_application_insights.appinsights.instrumentation_key
 }
--- key-vault.tf
+++ key-vault.tf
@@ -1,27 +1,27 @@
 module "sscs-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "sscs-${var.env}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
-  tenant_id               = "${var.tenant_id}"
-  object_id               = "${var.jenkins_AAD_objectId}"
-  resource_group_name     = "${azurerm_resource_group.rg.name}"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
   product_group_object_id = "87099fce-881e-4654-88d2-7c36b634e622"
-  common_tags             = "${local.tags}"
-  location                = "${var.location}"
+  common_tags             = local.tags
+  location                = var.location
 
   create_managed_identity    = true
 }
 
 data "azurerm_key_vault" "sscs_key_vault" {
   name                = "sscs-${var.env}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 output "vaultName" {
-  value = "${module.sscs-vault.key_vault_name}"
+  value = module.sscs-vault.key_vault_name
 }
 
 output "vaultUri" {
-  value = "${module.sscs-vault.key_vault_uri}"
+  value = module.sscs-vault.key_vault_uri
 }
