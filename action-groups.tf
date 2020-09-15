5d8fbbee5d18eb8aecdc03dcadc6689ebd13517d
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2020-09-15 10:23:02 +0100

    change vault_uri to key_vault_id

--- action-groups.tf
+++ action-groups.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault_secret" "sscs_failure_email_secret" {
   name      = "sscs-failure-email-to"
-  vault_uri = module.sscs-vault.key_vault_uri
+  vault_uri = module.sscs-vault.key_vault_id
 }
 
 module "sscs-fail-action-group" {
