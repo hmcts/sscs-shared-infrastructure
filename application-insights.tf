130a8d649ac84af54723c274b9725a76f3b0f66f
Tim Jacomb <tim.jacomb@hmcts.net>
2020-09-21 10:44:03 +0100

    Migrate app insights to UK South

--- application-insights.tf
+++ application-insights.tf
@@ -1,15 +1,10 @@
-variable "appinsights_location" {
-  default     = "West Europe"
-  description = "Location for Application Insights"
-}
-
 variable "appinsights_application_type" {
   default     = "web"
   description = "Type of Application Insights (Web/Other)"
 }
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
-  location            = var.appinsights_location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.appinsights_application_type
 }
