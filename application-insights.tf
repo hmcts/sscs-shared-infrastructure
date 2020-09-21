cbea5be5225dbc8186ef58d275a05f19b1e93b8d
Tim Jacomb <tim.jacomb@hmcts.net>
2020-09-21 10:49:31 +0100

    Migrate app insights to UK South (#101)

--- application-insights.tf
+++ application-insights.tf
@@ -1,17 +1,18 @@
-variable "appinsights_location" {
-  default     = "West Europe"
-  description = "Location for Application Insights"
-}
-
 variable "appinsights_application_type" {
   default     = "web"
   description = "Type of Application Insights (Web/Other)"
 }
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
-  location            = var.appinsights_location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.appinsights_application_type
+}
+
+resource "azurerm_key_vault_secret" "app_insights_key" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = module.sscs-vault.key_vault_id
 }
 
 output "appInsightsInstrumentationKey" {
