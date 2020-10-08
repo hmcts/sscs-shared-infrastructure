c52a4b7533e942594e0844d85cc97552583acd11
Ben Neill <benjaminrneill@gmail.com>
2020-10-08 23:02:06 +0100

    bump sku in AAT

--- servicebus.tf
+++ servicebus.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "servicebus-namespace" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=premium"
   name                = local.servicebus_namespace_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
