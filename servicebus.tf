71c303e43c7ce39d888a37139db55dab93d0c667
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-06-21 10:25:45 +0100

    set capacity to 2?

--- servicebus.tf
+++ servicebus.tf
@@ -14,7 +14,7 @@ module "servicebus-namespace" {
   env                 = var.env
   common_tags         = local.tags
   sku                 = "Premium"
-  capacity            = 1
+  capacity            = 2
 }
 
 module "evidenceshare-topic" {
