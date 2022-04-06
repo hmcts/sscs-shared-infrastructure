cf7723316671e0c0272852ae3d71dcbc2252a36c
Enda Kelly <enda.kelly@hmcts.net>
2022-04-06 14:21:20 +0100

    update provider name syntax

--- servicebus.tf
+++ servicebus.tf
@@ -8,7 +8,7 @@ locals {
 
 module "servicebus-namespace" {
   providers = {
-    azurerm.private-endpoint = azurerm.private-endpoint
+    azurerm.private_endpoint = azurerm.private_endpoint
   }
   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
   name                = local.servicebus_namespace_name
