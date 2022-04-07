9c519fb08acc54ab016fbd1d0ebe456f48453329
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-07 13:27:45 +0100

    DTSPO-6371 - stop using deprecated map function

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ locals {
   tags = "${
     merge(
       var.common_tags,
-      map(
-        "Team Contact", "#sscs",
-        "Team Name", "SSCS Team"
-      )
+      tomap({
+        "Team Contact" = "#sscs"
+        "Team Name" = "SSCS Team"
+      })
     )}"
 }
 resource "azurerm_resource_group" "rg" {
