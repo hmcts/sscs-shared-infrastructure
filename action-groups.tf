8b6a1096cab6f87d2a6613e64ffe2683f5f5c5d7
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-06-13 09:11:30 +0100

    SSCS-10638-DeadLetterQueueAlert

--- action-groups.tf
+++ action-groups.tf
@@ -26,3 +26,20 @@ module "sscs-fail-action-group-slack" {
   email_receiver_name    = "SSCS Alerts"
   email_receiver_address = "sscs-prod-monitoring-aaaac7vjnaaknbv4uixozinjim@hmcts-reform.slack.com"
 }
+
+data "azurerm_key_vault_secret" "sscs_dead_letter_email_secret" {
+  name         = "sscs-deal-letter-email-to"
+  key_vault_id = module.sscs-vault.key_vault_id
+}
+
+module "sscs-dead-letter-action-group" {
+  source   = "git@github.com:hmcts/cnp-module-action-group"
+  location = "global"
+  env      = var.env
+
+  resourcegroup_name     = azurerm_resource_group.rg.name
+  action_group_name      = "SSCS Dead Letter Queue Alert - ${var.env}"
+  short_name             = "SSCS_DLet_alert"
+  email_receiver_name    = "SSCS Alerts"
+  email_receiver_address = data.azurerm_key_vault_secret.sscs_dead_letter_email_secret.value
+}
