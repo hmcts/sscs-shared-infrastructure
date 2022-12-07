c68f23101b8659cf8bfdf529d9071b3d4572b3f5
Simran Sian <simran.sian@hmcts.net>
2022-12-07 11:04:56 +0000

    Add azure storage account with SFTP support

--- sftp-storage-account.tf
+++ sftp-storage-account.tf
@@ -27,10 +27,15 @@ data "azurerm_key_vault_secret" "sftp_user_ssh_key" {
   key_vault_id = module.sscs-vault.key_vault_id
 }
 
+data "azurerm_key_vault_secret" "sftp_user_name" {
+  name         = "sftp-user-name"
+  key_vault_id = module.sscs-vault.key_vault_id
+}
+
 # Workaround until azurerm_storage_account supports the ability to create local users
 resource "azapi_resource" "add_local_user" {
   type = "Microsoft.Storage/storageAccounts/localUsers@2021-09-01"
-  name = "testuser3"
+  name = "${azurerm_key_vault_secret.sftp_user_name.value}"
   parent_id = azurerm_storage_account.sftp_storage.id
 
   body = jsonencode({
