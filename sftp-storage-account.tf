5f204225e7bc39f847a1204c5f0e12a59bef19c1
Simran Sian <simran.sian1@justice.gov.uk>
2023-02-27 13:00:47 +0000

    update sftp-storage-account

--- sftp-storage-account.tf
+++ sftp-storage-account.tf
@@ -16,6 +16,7 @@ module "sftp_storage_account" {
   storage_account_name     = "sscssftp${var.env}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
+  account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   enable_hns               = "true"
