16e92cedbef834c4f3a7ef429eadefd107b597ac
ssian2 <88483780+ssian2@users.noreply.github.com>
2023-03-06 14:58:09 +0000

    Update storage account permissions (#148)
    * Grant blob contributor access to SFTP storage account of AAD group
    
    * Move storage account to CNP module
    
    * update sftp-storage-account
    
    * update sftp-storage-account
    
    * update sftp-storage-account
    
    * Change cnp module ref
    
    * update sftp-storage-account.tf
    
    * Fix foreach
    
    * rename storage account var
    
    * Fix deployment errors

--- sftp-storage-account.tf
+++ sftp-storage-account.tf
@@ -1,4 +1,4 @@
-
+/*
 resource "azurerm_storage_account" "sftp_storage" {
   name                     = "sscssftp${var.env}"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -8,16 +8,41 @@ resource "azurerm_storage_account" "sftp_storage" {
   is_hns_enabled           = true
   sftp_enabled             = true
   tags                     = local.tags
+}*/
+
+module "sftp_storage" {
+  source = "git@github.com:hmcts/cnp-module-storage-account?ref=feature/sftp-support"
+  env                      = var.env
+  storage_account_name     = "sscssftp${var.env}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_hns               = "true"
+  enable_sftp              = "true"
+  
+  managed_identity_object_id = var.sftp_access_AAD_objectId
+  role_assignments = [
+    "Storage Blob Data Contributor"
+  ]
+
+  sa_subnets = var.sftp_allowed_sa_subnets
+
+  team_name    = "SSCS Team"
+  team_contact = "#sscs"
+  common_tags  = var.common_tags
 }
 
 resource "azurerm_storage_container" "sftp_container" {
   name                  = "upload"
-  storage_account_name  = azurerm_storage_account.sftp_storage.name
+  storage_account_name  = module.sftp_storage.storageaccount_name
   container_access_type = "private"
 }
 
-data "azurerm_key_vault_secret" "sftp_user_key" {
-  name         = "sftp-user-pub-key"
+data "azurerm_key_vault_secret" "sftp_user_keys" {
+  for_each     = toset(var.sftp_allowed_key_secrets)
+  name         = each.value #"sftp-user-pub-key"
   key_vault_id = module.sscs-vault.key_vault_id
 }
 
@@ -30,7 +55,7 @@ data "azurerm_key_vault_secret" "sftp_user_name" {
 resource "azapi_resource" "add_local_user" {
   type = "Microsoft.Storage/storageAccounts/localUsers@2021-09-01"
   name = "${data.azurerm_key_vault_secret.sftp_user_name.value}"
-  parent_id = azurerm_storage_account.sftp_storage.id
+  parent_id = module.sftp_storage.storageaccount_id
 
   body = jsonencode({
     properties = {
@@ -43,9 +68,9 @@ resource "azapi_resource" "add_local_user" {
       ],
       "hasSshPassword" : true,
       "sshAuthorizedKeys" : [
-        {
-          "description" : "key name",
-          "key" : "${data.azurerm_key_vault_secret.sftp_user_key.value}"
+        for k in data.azurerm_key_vault_secret.sftp_user_keys : {
+          "description": k.name,
+          "key": k.value
         }
       ],
       "homeDirectory" : "upload"
