717bef54b7390d2439f11da61859246f58b380b6
Simran Sian <simran.sian1@justice.gov.uk>
2023-04-13 11:39:27 +0100

    Reorder sftp-storage-account.tf

--- sftp-storage-account.tf
+++ sftp-storage-account.tf
@@ -1,3 +1,25 @@
+locals {
+  private_endpoint_rg_name   = var.businessArea == "sds" ? "ss-${var.env}-network-rg" : "${var.businessArea}-${var.env}-network-rg"
+  private_endpoint_vnet_name = var.businessArea == "sds" ? "ss-${var.env}-vnet" : "${var.businessArea}-${var.env}-vnet"
+}
+
+# CFT only, on SDS remove this provider
+provider "azurerm" {
+  alias           = "private_endpoints"
+  subscription_id = var.aks_subscription_id
+  features {}
+  skip_provider_registration = true
+}
+
+data "azurerm_subnet" "private_endpoints" {
+  # CFT only you will need to provide an extra provider, uncomment the below line, on SDS remove this line and the next
+  provider = azurerm.private_endpoints
+
+  resource_group_name  = local.private_endpoint_rg_name
+  virtual_network_name = local.private_endpoint_vnet_name
+  name                 = "private-endpoints"
+}
+
 module "sftp_storage" {
   source = "git@github.com:hmcts/cnp-module-storage-account?ref=fix/private-endpoint-provider"
   env                      = var.env
@@ -66,49 +88,4 @@ resource "azapi_resource" "add_local_user" {
       "homeDirectory" : "upload"
     }
   })
-}
-
-locals {
-  private_endpoint_rg_name   = var.businessArea == "sds" ? "ss-${var.env}-network-rg" : "${var.businessArea}-${var.env}-network-rg"
-  private_endpoint_vnet_name = var.businessArea == "sds" ? "ss-${var.env}-vnet" : "${var.businessArea}-${var.env}-vnet"
-}
-
-# CFT only, on SDS remove this provider
-provider "azurerm" {
-  alias           = "private_endpoints"
-  subscription_id = var.aks_subscription_id
-  features {}
-  skip_provider_registration = true
-}
-
-data "azurerm_subnet" "private_endpoints" {
-  # CFT only you will need to provide an extra provider, uncomment the below line, on SDS remove this line and the next
-  provider = azurerm.private_endpoints
-
-  resource_group_name  = local.private_endpoint_rg_name
-  virtual_network_name = local.private_endpoint_vnet_name
-  name                 = "private-endpoints"
-}
-
-resource "azurerm_private_endpoint" "sftp_blob_endpoint" {
-  provider = azurerm.private_endpoints
-
-  name                = module.sftp_storage.storageaccount_id
-  resource_group_name = local.private_endpoint_rg_name
-  location            = var.location
-  subnet_id           = data.azurerm_subnet.private_endpoints.id
-
-  private_service_connection {
-    name                           = module.sftp_storage.storage_account_name
-    is_manual_connection           = false
-    private_connection_resource_id = module.sftp_storage.storageaccount_id
-    subresource_names              = ["blob"]
-
-    private_dns_zone_group {
-      name                 = "endpoint-dnszonegroup"
-      private_dns_zone_ids = ["/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.blob.core.windows.net"]
-    }
-
-    tags = var.common_tags
-  }
 }
