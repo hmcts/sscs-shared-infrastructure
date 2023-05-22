34e360a159a34968882fac3fa57b6454ba185b66
Andrei Trushkov <andrei.trushkov@hmcts.net>
2023-05-22 10:41:18 +0100

    app insight upgrade resource

--- application-insights.tf
+++ application-insights.tf
@@ -26,3 +26,9 @@ output "appInsightsInstrumentationKey" {
   sensitive = true
   value     = azurerm_application_insights.appinsights.instrumentation_key
 }
+
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.sscs-vault.key_vault_id
+}
