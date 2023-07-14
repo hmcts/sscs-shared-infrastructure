7f1b7979574c88f2de1387c1cde8c370505a7254
rhodrif <132342364+rhodrif@users.noreply.github.com>
2023-07-14 13:52:50 +0100

    SSCS-11781 sscssftpprod blob update alert (#152)
    * added diagnostic settings to storage account
    
    * tweaked source path for law module
    
    * tweaked source path for law module
    
    * added alert config
    
    * added alert config
    
    * added alert module readme

---
+++ modules/az_metric_alert/main.tf
@@ -0,0 +1,23 @@
+data "template_file" "actiongrouptemplate" {
+  template = file("${path.module}/templates/metric-alert.json")
+}
+
+resource "azurerm_resource_group_template_deployment" "metric-alert" {
+  template_content    = data.template_file.actiongrouptemplate.rendered
+  resource_group_name = var.resourcegroup_name
+  name                = var.arm_deployment_name
+  deployment_mode     = "Incremental"
+
+  parameters_content = jsonencode({
+    actionGroupsExternalId    = { value = var.action_groups_external_id }
+    alertDescription          = { value = var.alert_description }
+    alertEvaluationFrequency  = { value = var.alert_evaluation_frequency }
+    alertQuery                = { value = var.alert_query }
+    alertSeverity             = { value = var.alert_severity }
+    alertWindowSize           = { value = var.alert_window_size }
+    location                  = { value = var.location }
+    scheduledQueryRulesName   = { value = var.scheduled_query_rules_name }
+    storageAccountsExternalId = { value = var.storage_accounts_external_id }
+  })
+
+}
---
+++ sftp-storage-account-alert.tf
@@ -0,0 +1,19 @@
+# Create metric alert.
+module "metric-alert" {
+  depends_on          = [module.sscs-fail-action-group, azurerm_monitor_diagnostic_setting.mds1, azurerm_monitor_diagnostic_setting.mds2]
+  source              = "./modules/az_metric_alert"
+  resourcegroup_name  = azurerm_resource_group.rg.name
+  arm_deployment_name = "sscs-fail-alert-sscssftpprod-email"
+
+  # ARM parameters
+  action_groups_external_id    = "/subscriptions/${var.subscription}/resourceGroups/${azurerm_resource_group.rg.name}/providers/microsoft.insights/actionGroups/${module.sscs-fail-action-group.action_group_name}"
+  alert_description            = "The following storage account / blob has been updated:\n\n'sscssftpprod\\upload\\incoming\\failed'"
+  alert_evaluation_frequency   = "PT5M"
+  alert_query                  = "StorageBlobLogs | where TimeGenerated > ago(5m) | where OperationName == 'PutBlob' | where ObjectKey has \"upload/incoming/failed\""
+  alert_severity               = "2"
+  alert_window_size            = "PT5M"
+  location                     = azurerm_resource_group.rg.location
+  scheduled_query_rules_name   = "sscs-fail-alert-sscssftpprod-email"
+  storage_accounts_external_id = "/subscriptions/${var.subscription}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${module.sftp_storage.storageaccount_name}"
+
+}
