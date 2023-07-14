eef4904b1746f8901c4a798e133ab7703024eee1
rhodrif <rhodri.freer@version1.com>
2023-07-14 17:02:04 +0100

    fix for duplicate deployment name

--- sftp-storage-account-alert.tf
+++ sftp-storage-account-alert.tf
@@ -3,7 +3,7 @@ module "metric-alert" {
   depends_on          = [module.sscs-fail-action-group, azurerm_monitor_diagnostic_setting.mds1, azurerm_monitor_diagnostic_setting.mds2]
   source              = "./modules/az_metric_alert"
   resourcegroup_name  = azurerm_resource_group.rg.name
-  arm_deployment_name = "sscs-fail-alert-sscssftpprod-email"
+  arm_deployment_name = "sscs-fail-alert-sscssftp-email"
 
   # ARM parameters
   action_groups_external_id    = "/subscriptions/${module.log_analytics_workspace.subscription_id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/microsoft.insights/actionGroups/${module.sscs-fail-action-group.action_group_name}"
@@ -13,7 +13,7 @@ module "metric-alert" {
   alert_severity               = "2"
   alert_window_size            = "PT5M"
   location                     = azurerm_resource_group.rg.location
-  scheduled_query_rules_name   = "sscs-fail-alert-sscssftpprod-email"
+  scheduled_query_rules_name   = "sscs-fail-alert-sscssftp-email"
   storage_accounts_external_id = "/subscriptions/${module.log_analytics_workspace.subscription_id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${module.sftp_storage.storageaccount_name}"
   common_tags                  = var.common_tags
 
