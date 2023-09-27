a21bbd44d7589619532c3fbb7db036dfc1540b07
nicholas chang <nicholasc001@hotmail.com>
2023-09-27 16:13:23 +0100

    Revert "added slack alert (#157)"
    This reverts commit f9e3b6f26f33f5bff8cfaf74e6fb72e543545cbd.

--- sftp-storage-account-alert.tf
+++ sftp-storage-account-alert.tf
@@ -1,4 +1,4 @@
-# Create metric alert - email.
+# Create metric alert.
 module "metric-alert" {
   depends_on          = [module.sscs-fail-action-group, azurerm_monitor_diagnostic_setting.mds1, azurerm_monitor_diagnostic_setting.mds2]
   source              = "./modules/az_metric_alert"
@@ -14,27 +14,6 @@ module "metric-alert" {
   alert_window_size            = "PT5M"
   location                     = azurerm_resource_group.rg.location
   scheduled_query_rules_name   = "sscs-fail-alert-sscssftp-email"
-  storage_accounts_external_id = "/subscriptions/${module.log_analytics_workspace.subscription_id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${module.sftp_storage.storageaccount_name}"
-  common_tags                  = var.common_tags
-
-}
-
-# Create metric alert - slack.
-module "metric-alert-slack" {
-  depends_on          = [module.sscs-fail-action-group, azurerm_monitor_diagnostic_setting.mds1, azurerm_monitor_diagnostic_setting.mds2]
-  source              = "./modules/az_metric_alert"
-  resourcegroup_name  = azurerm_resource_group.rg.name
-  arm_deployment_name = "sscs-fail-alert-sscssftp-slack"
-
-  # ARM parameters
-  action_groups_external_id    = "/subscriptions/${module.log_analytics_workspace.subscription_id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/microsoft.insights/actionGroups/${module.sscs-fail-action-group-slack.action_group_name}"
-  alert_description            = "The following storage account / blob has been updated:\n\n'sscssftpprod\\upload\\incoming\\failed'"
-  alert_evaluation_frequency   = "PT5M"
-  alert_query                  = "StorageBlobLogs | where TimeGenerated > ago(5m) | where OperationName == 'PutBlob' | where ObjectKey has \"upload/incoming/failed\""
-  alert_severity               = "2"
-  alert_window_size            = "PT5M"
-  location                     = azurerm_resource_group.rg.location
-  scheduled_query_rules_name   = "sscs-fail-alert-sscssftp-slack"
   storage_accounts_external_id = "/subscriptions/${module.log_analytics_workspace.subscription_id}/resourceGroups/${azurerm_resource_group.rg.name}/providers/Microsoft.Storage/storageAccounts/${module.sftp_storage.storageaccount_name}"
   common_tags                  = var.common_tags
 
