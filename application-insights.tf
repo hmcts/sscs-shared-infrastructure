d5faec3481701a66c8226a0fcf0f36fef3ce2169
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-01-02 10:18:02 +0000

    DTSPO-15909 - migrated Application Insights

--- application-insights.tf
+++ application-insights.tf
@@ -1,19 +1,25 @@
-variable "appinsights_application_type" {
-  default     = "web"
-  description = "Type of Application Insights (Web/Other)"
-}
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.env}"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = var.appinsights_application_type
 
-  tags = local.tags
+module "application_insights" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+
+  env     = var.env
+  product = var.product
+  name    = var.product
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  common_tags = local.tags
+}
+
+
+moved {
+  from = azurerm_application_insights.appinsights
+  to   = module.application_insights.azurerm_application_insights.this
 }
 
 resource "azurerm_key_vault_secret" "app_insights_key" {
   name         = "AppInsightsInstrumentationKey"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
+  value        = module.application_insights.instrumentation_key
   key_vault_id = module.sscs-vault.key_vault_id
 
   content_type = "terraform-managed,service-bus"
@@ -24,11 +30,11 @@ resource "azurerm_key_vault_secret" "app_insights_key" {
 
 output "appInsightsInstrumentationKey" {
   sensitive = true
-  value     = azurerm_application_insights.appinsights.instrumentation_key
+  value     = module.application_insights.instrumentation_key
 }
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = module.application_insights.connection_string
   key_vault_id = module.sscs-vault.key_vault_id
 }
