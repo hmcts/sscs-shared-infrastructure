e8c05c296b72b4a36a21d6793f7f8e10719c2c24
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-01-24 16:16:30 +0000

    testing

--- key-vault.tf
+++ key-vault.tf
@@ -1,5 +1,5 @@
 module "sscs-vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=jh-testing"
   name                    = "sscs-${var.env}"
   product                 = var.product
   env                     = var.env
