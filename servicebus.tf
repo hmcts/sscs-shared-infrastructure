da1c93e63f160fc35203b314fbf725e18057ca58
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2024-06-19 10:19:18 +0100

    SSCSSI-348 Add correspondence topic to existing namespace (#175)
    * SSCSSI-348 Add correspondence topic to existing queue
    
    * bump azurerm version

--- servicebus.tf
+++ servicebus.tf
@@ -1,5 +1,7 @@
 locals {
   evidenceshare_topic_name        = "${var.product}-evidenceshare-topic-${var.env}"
+  correspondence_topic_name       = "${var.product}-correspondence-topic-${var.env}"
+  correspondence_subscription_name= "${var.product}-correspondence-subscription-${var.env}"
   evidenceshare_subscription_name = "${var.product}-evidenceshare-subscription-${var.env}"
   notifications_subscription_name = "${var.product}-notifications-subscription-${var.env}"
   servicebus_namespace_name       = "${var.product}-servicebus-${var.env}"
@@ -97,5 +99,37 @@ resource "azurerm_key_vault_secret" "evidence_share_topic_primary_shared_access_
   content_type = "terraform-managed,service-bus"
   tags = merge(local.tags, {
     "source" : "Service Bus ${module.evidenceshare-topic.name}"
+  })
+}
+
+module "correspondence-topic" {
+  source                                  = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+  name                                    = local.correspondence_topic_name
+  namespace_name                          = local.servicebus_namespace_name
+  resource_group_name                     = local.resource_group_name
+  requires_duplicate_detection            = true
+  duplicate_detection_history_time_window = "PT1H"
+  max_size_in_megabytes                   = 2048
+  max_message_size_in_kilobytes           = var.max_message_size_in_kilobytes
+}
+
+module "correspondence-subscription" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                = local.correspondence_subscription_name
+  namespace_name      = local.servicebus_namespace_name
+  resource_group_name = local.resource_group_name
+  topic_name          = local.correspondence_topic_name
+
+  depends_on = [module.correspondence-topic]
+}
+
+resource "azurerm_key_vault_secret" "correspondence_topic_primary_shared_access_key" {
+  name         = "correspondence-topic-shared-access-key"
+  value        = module.correspondence-topic.primary_send_and_listen_shared_access_key
+  key_vault_id = module.sscs-vault.key_vault_id
+
+  content_type = "terraform-managed,service-bus"
+  tags = merge(local.tags, {
+    "source" : "Service Bus ${module.correspondence-topic.name}"
   })
 }
